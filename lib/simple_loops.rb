1f92b0a96a7255e2aad2587e2f550312058dffa6
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-06 15:40:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,7 +27,7 @@ def return_string_array (array)
   count = 0 
   new_arr = []
   while count < array.length do 
-   new_array << array[count].to_s
+   new_arr << array[count].to_s
     count += 1 
   end
   new_arr
